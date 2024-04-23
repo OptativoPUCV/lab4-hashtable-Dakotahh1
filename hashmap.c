15c9d2fd66ccd924c2fccf8672719928a24e9267
Dakotah <163438035+Dakotahh1@users.noreply.github.com>
2024-04-23 04:31:16 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -49,18 +49,18 @@ void insertMap(HashMap * map, char * key, void * value) {
   }
   else
   {
-    while(map->buckets[posicion]!=NULL || map->buckets[posicion]->key==NULL)
+    while(map->buckets[posicion]!=NULL && map->buckets[posicion]->key==NULL)
       {
         if(is_equal(map->buckets[posicion]->key,key)){
           map->buckets[posicion]->value=value;
           return;
         }
-        else if(posicion==map->capacity) posicion=0;
         posicion++;
         
       }
     map->buckets[posicion]=par;
     map->size++;
+    map->current = posicion
   }
 }
 
