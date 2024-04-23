1d4db21da7a83b1e630fadd4d68492cf5f3ec0d0
Dakotah <163438035+Dakotahh1@users.noreply.github.com>
2024-04-23 05:40:21 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -97,7 +97,12 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-  
+  for (unsigned long i = 0; i < map->capacity; i++) {
+    if (map->buckets[i] != NULL && map->buckets[i]->key != NULL) {
+        map->current = i;
+        return map->buckets[i];
+    }
+  }
   return NULL;
 }
 
